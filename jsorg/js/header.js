781fecc0d49b1667a55b0f6bd20dab7a565d0863
jackhwl <jackhwl@hotmail.com>
2018-07-06 19:01:59 -0400

    Kyle Simpson Organizing Javascript Functionality

---
+++ jsorg/js/header.js
@@ -0,0 +1,17 @@
+// put event handlers for header links here
+$(document).ready(function(){
+    var $modal = $("[rel='js-modal']");
+    $("[rel='js-controls']").on("click", "[rel*='js-']", function(evt){
+        evt.preventDefault();
+        evt.stopPropagation();
+        evt.stopImmediatePropagation();
+
+        var url = $(evt.target).attr("href");
+
+        $.ajax(url, { dataType: "text" })
+        .then(function(contents){
+            $modal.html(contents).show();
+        });
+    })
+
+});
