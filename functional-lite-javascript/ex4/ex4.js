1c6604a4e1a5cedd35d22bf9f413f11ace7dfdaa
jackhwl <jackhwl@hotmail.com>
2018-08-02 22:09:48 -0400

    ex4 for-loop solution

--- functional-lite-javascript/ex4/ex4.js
+++ functional-lite-javascript/ex4/ex4.js
@@ -1,1 +1,46 @@
 // code here! :)
+// function foo(){
+//     return 42;
+// }
+
+// function bar() {
+//     return 10;
+// }
+
+function foo(x) {
+    return function() {
+        return x;
+    }
+}
+
+function add(x, y) {
+    return x + y;
+}
+
+function add2(fn1, fn2) {
+    return add(fn1(), fn2());
+}
+
+function addn(arr) {
+    var sum = 0;
+    for(var i = 0; i < arr.length; i++) {
+        sum = add2(foo(arr[i]), foo(sum));
+    }
+    return sum;
+}
+
+function addn(arr) {
+	if (vals.length > 2) {
+		return addn(
+			[
+				function() {
+					return add2(vals[0],vals[1]);
+				}
+			]
+			.concat(vals.slice(2))
+		);
+	}
+    return add2(vals[0],vals[1]);
+}
+
+//add(foo(), bar())
