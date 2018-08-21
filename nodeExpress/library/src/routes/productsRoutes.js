2f6a5b53f15838f2ae1a179d0eb8b26f4283252f
jackhwl <jackhwl@hotmail.com>
2018-08-20 21:54:48 -0400

    mongodb product

--- nodeExpress/library/src/routes/productsRoutes.js
+++ nodeExpress/library/src/routes/productsRoutes.js
@@ -1,58 +1,73 @@
 
 const express = require('express');
+const { MongoClient, ObjectID } = require('mongodb');
+const debug = require('debug')('app:productsRouter');
 
 const productsRouter = express.Router();
 
 function router(nav) {
-    const products = [
-        {
-            title: 'Coffees & Teas',
-            genre: 'Blended to Perfection',
-            description: 'We take pride in our work, and it shows. Every time you order a beverage from us, we guarantee that it will be an experience worth having. Whether it\'s our world famous Venezuelan Cappuccino, a refreshing iced herbal tea, or something as simple as a cup of speciality sourced black coffee, you will be coming back for more.',
-            image: '/img/products-01.jpg',
-            read: false
-        },
-        {
-            title: 'Bakery & Kitchen',
-            genre: 'Delicious Treats, Good Eats',
-            description: 'Our seasonal menu features delicious snacks, baked goods, and even full meals perfect for breakfast or lunchtime. We source our ingredients from local, oragnic farms whenever possible, alongside premium vendors for specialty goods.',
-            image: '/img/products-02.jpg',
-            read: false
-        },
-        {
-            title: 'Bulk Speciality Blends',
-            genre: 'From Around the World',
-            description: 'Travelling the world for the very best quality coffee is something take pride in. When you visit us, you\'ll always find new blends from around the world, mainly from regions in Central and South America. We sell our blends in smaller to large bulk quantities. Please visit us in person for more details.',
-            image: '/img/products-03.jpg',
-            read: false
-        }
-    ];
-       productsRouter.route('/')
+    productsRouter.route('/')
         .get((req, res) => {
-            res.render(
-                'productListView',
-                {
-                    title: 'Bridletowne Park Church',
-                    nav,
-                    products
+            const url = 'mongodb://localhost:27017';
+            const dbName = 'libraryApp';
+            (async function mongo() {
+                let client;
+                try {
+                    client = await MongoClient.connect(url);
+                    debug('Connected correctly to server');
+
+                    const db = client.db(dbName);
+
+                    const collection = await db.collection('products');
+                    
+                    const products = await collection.find().toArray();
+                    res.render(
+                        'productListView',
+                        {
+                            title: 'Bridletowne Park Church',
+                            nav,
+                            products
+                        }
+                    );
+                } catch (err) {
+                    debug(err.stack);
                 }
-            );
+                client.close();
+            }());
         });
        productsRouter.route('/:id')
         .get((req, res) => {
             const { id } = req.params;
-            res.render(
-                'productView',
-                {
-                    title: 'Bridletowne Park Church',
-                    nav,
-                    product: products[id]
+            const url = 'mongodb://localhost:27017';
+            const dbName = 'libraryApp';
+            (async function mongo() {
+                let client;
+                try {
+                    client = await MongoClient.connect(url);
+                    debug('Connected correctly to server');
+
+                    const db = client.db(dbName);
+
+                    const collection = await db.collection('products');
+
+                    const product = await collection.findOne({ _id: new ObjectID(id) });
+                    res.render(
+                        'productView',
+                        {
+                            title: 'Bridletowne Park Church',
+                            nav,
+                            product
+                        }
+                    );
+                        } catch (err) {
+                    debug(err.stack);
                 }
-            );
+                client.close();
+            }());
         });
        return productsRouter;
-    
     }
+    
 /* mssql 
 const sql = require('mssql');
 const debug = require('debug')('app:productsRoutes');
