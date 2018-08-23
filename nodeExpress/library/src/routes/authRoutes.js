3496822d45e418de23ae4886200e0eb719a5660c
jackhwl <jackhwl@hotmail.com>
2018-08-22 23:49:02 -0400

    authentication

--- nodeExpress/library/src/routes/authRoutes.js
+++ nodeExpress/library/src/routes/authRoutes.js
@@ -4,13 +4,44 @@ const debug = require('debug')('app:authRoutes');
 
 const authRouter = express.Router();
 
-function router() {
+function router(nav) {
     authRouter.route('/signUp')
     .post((req, res) => {
-        debug(req.body);
-        res.json(req.body);
-    });
+        const { username, password } = req.body;
+        const url = 'mongodb://localhost:27017';
+        const dbName = 'libraryApp';
+        (async function addUser() {
+            let client;
+            try {
+                client = await MongoClient.connect(url);
+                debug('Connected correctly to server');
 
+                const db = client.db(dbName);
+                const col = db.collection('users');
+                const user = { username, password };
+                const results = await col.insertOne(user);
+                debug(results);
+            } catch (err) {
+                debug(err);
+            }
+        }());
+        debug(req.body);
+        // create user
+        req.login(req.body, () => {
+            res.redirect('/auth/profile');
+        });
+    });
+    authRouter.route('/signin')
+        .get((req, res) => {
+            res.render('signin', {
+                nav,
+                title: 'Sign In'
+            });
+        });
+    authRouter.route('/profile')
+        .get((req, res) => {
+            res.json(req.user);
+        });
     return authRouter;
 }
 
