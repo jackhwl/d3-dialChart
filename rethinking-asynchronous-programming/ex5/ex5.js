c07ef22c8150de282a8b69360c0d7fa20d934d48
jackhwl <jackhwl@hotmail.com>
2018-09-09 22:34:50 -0400

    ex5 use ASQ library

---
+++ rethinking-asynchronous-programming/ex5/ex5.js
@@ -0,0 +1,43 @@
+function fakeAjax(url,cb) {
+	var fake_responses = {
+		"file1": "The first text",
+		"file2": "The middle text",
+		"file3": "The last text"
+	};
+	var randomDelay = (Math.round(Math.random() * 1E4) % 8000) + 1000;
+
+	console.log("Requesting: " + url);
+
+	setTimeout(function(){
+		cb(fake_responses[url]);
+	},randomDelay);
+}
+
+function output(text) {
+	console.log(text);
+}
+
+// **************************************
+
+// var getFile = ASQ.wrap(fakeAjax, { simplecb: true});
+
+function getFile(file) {
+	// what do we do here?
+	return ASQ(function(done){
+		fakeAjax(file, done);
+	})
+}
+
+// request an array of files at once in "parallel"
+// ???
+var s1 = getFile('file1');
+var s2 = getFile('file2');
+var s3 = getFile('file3');
+
+s1.val(output)
+.seq(s2)
+.val(output)
+.seq(s3)
+.val(output)
+.val(() => output('Complete!'));
+
