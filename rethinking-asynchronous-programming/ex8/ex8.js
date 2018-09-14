07ab799220ba653021c7f52a02def5a204621916
jackhwl <jackhwl@hotmail.com>
2018-09-14 08:23:22 -0400

    Observables with ASQ

---
+++ rethinking-asynchronous-programming/ex8/ex8.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	var $btn = $("#btn"),
+		$list = $("#list"),
+		clicks = ASQ.react.of(),
+		timer = ASQ.react.of();
+
+	$btn.click(function(evt){
+		clicks.push(evt);
+	});
+
+	setInterval(function(){
+		timer.push();
+	}, 1000);
+
+	var msgs = ASQ.react.all(clicks, timer);
+	msgs.val(function(){
+		$list.append($("<div>Clicked!</div>"));
+	});
+
+	// TODO: setup sampled sequence, populate $list
+});
