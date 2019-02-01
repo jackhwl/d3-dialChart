aec27e2b10c3537dca742ffa80a0d27749e2a057
jackhwl <jackhwl@hotmail.com>
2019-02-01 08:31:58 -0500

    promise with azure ms-sqlserver

--- Building_Web_Applications_with_NodeJs_and_Express_40/library/app.js
+++ Building_Web_Applications_with_NodeJs_and_Express_40/library/app.js
@@ -3,10 +3,22 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const sql = require('mssql');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+const config = {
+  user: 'library',
+  password: 'Hkksxds1',
+  server: 'pslibrary21.database.windows.net', // You can use 'localhost\\instance' to connect to named instance
+  database: 'PSLibrary',
+
+  options: {
+    encrypt: true // Use this if you're on Windows Azure
+  }
+};
+sql.connect(config).catch(err => debug(err));
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
@@ -15,18 +27,18 @@ app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist'))
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
-const nav = [{link: '/books', title: 'Book'},
-{link: '/authors', title: 'Author'}];
+const nav = [{ link: '/books', title: 'Book' },
+  { link: '/authors', title: 'Author' }];
 
 const bookRouter = require('./src/routes/bookRoutes')(nav);
 
 app.use('/books', bookRouter);
 app.get('/', (req, res) => {
   res.render(
-    'index', 
-    { 
-      nav: [{link: '/books', title: 'Books'},
-        {link: '/authors', title: 'Authors'}], 
+    'index',
+    {
+      nav: [{ link: '/books', title: 'Books' },
+        { link: '/authors', title: 'Authors' }],
       title: 'Library'
     }
   );
--- Building_Web_Applications_with_NodeJs_and_Express_40/library/src/routes/bookRoutes.js
+++ Building_Web_Applications_with_NodeJs_and_Express_40/library/src/routes/bookRoutes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bookRouter = express.Router();
+const sql = require('mssql');
+const debug = require('debug')('app:bookRoutes');
 
 function router(nav) {
     const books = [
@@ -53,14 +55,19 @@ function router(nav) {
         }];
     bookRouter.route('/')
     .get((req, res) => {
+      const request = new sql.Request();
+      request.query('select * from books')
+      .then((result) => {
+        debug(result);
         res.render(
         'bookListView', 
         { 
             nav, 
             title: 'Library',
-            books
+            books: result.recordset
         });
     });
+  });
 
     bookRouter.route('/:id')
     .get((req, res) => {
