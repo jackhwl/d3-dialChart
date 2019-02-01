880b6aa847bf6a1971f679759715b670178e99e9
jackhwl <jackhwl@hotmail.com>
2019-02-01 14:56:52 -0500

    passport local-stratety

--- Building_Web_Applications_with_NodeJs_and_Express_40/library/app.js
+++ Building_Web_Applications_with_NodeJs_and_Express_40/library/app.js
@@ -3,6 +3,10 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const sql = require('mssql');
 
 const app = express();
@@ -20,6 +24,12 @@ const config = {
 };
 sql.connect(config).catch(err => debug(err));
 app.use(morgan('tiny'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({ secret: 'library' }));
+
+require('./src/config/passport.js')(app);
 // app.use((req, res, next) => {
 //   debug('my middleware');
 //   next();
@@ -38,10 +48,13 @@ const nav = [{ link: '/books', title: 'Book' },
 const bookRouter = require('./src/routes/bookRoutes')(nav);
 const mongoBookRouter = require('./src/routes/mongoBookRoutes')(nav);
 const adminRouter = require('./src/routes/adminRoutes')(nav);
+const authRouter = require('./src/routes/authRoutes')(nav);
 
 app.use('/books', bookRouter);
 app.use('/mongobooks', mongoBookRouter);
 app.use('/admin', adminRouter);
+app.use('/auth', authRouter);
+
 app.get('/', (req, res) => {
   res.render(
     'index',
---
+++ Building_Web_Applications_with_NodeJs_and_Express_40/library/src/routes/authRoutes.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const debug = require('debug')('app:authRoutes');
+
+const authRouter = express.Router();
+
+function router() {
+  authRouter.route('/signUp')
+    .post((req, res) => {
+    // create user
+      const { username, password } = req.body;
+      const url = 'mongodb://localhost:27017';
+      const dbName = 'library2App';
+      (async function addUser() {
+        let client;
+        try {
+          client = await MongoClient.connect(url);
+          debug('Connected correctly to server');
+          const db = client.db(dbName);
+          const collection = await db.collection('users');
+          const user = { username, password };
+          const results = await collection.insertOne(user);
+          debug(results);
+          req.login(results.ops[0], () => {
+            res.redirect('/auth/profile');
+          });
+        } catch (err) {
+          debug(err.stack);
+        }
+
+        client.close();
+      }());
+    });
+  authRouter.route('/profile')
+    .get((req, res) => {
+      res.json(req.user);
+    });
+  return authRouter;
+}
+
+module.exports = router;
