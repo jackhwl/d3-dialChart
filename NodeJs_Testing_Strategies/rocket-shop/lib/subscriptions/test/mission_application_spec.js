df288d5e6c51e248fd293ff4100528d0464ca9e7
jackhwl <jackhwl@hotmail.com>
2019-02-09 09:49:06 -0500

    mocha with moment

--- NodeJs_Testing_Strategies/rocket-shop/lib/subscriptions/test/mission_application_spec.js
+++ NodeJs_Testing_Strategies/rocket-shop/lib/subscriptions/test/mission_application_spec.js
@@ -1,7 +1,87 @@
 var assert = require("assert");
+var MembershipApplication = require('../membership_application');
 
-describe("Applying for mission", () => {
-    describe("Using valid email, first, last", () => {
-        it("let's them in");
+describe("Membership application requirements", () => {
+    var validApp;
+    before(() => {
+        validApp = new MembershipApplication({
+            first: "Test",
+            last: "User",
+            email: "test@test.com",
+            age: 30,
+            height: 66,
+            weight: 180
+        });
+    });
+
+    describe("Allication valid if...", () => {
+        it("all validators successful", () => {
+            assert(validApp.isValid());
+        });
+    });
+
+    describe("Allication invalid if...", () => {
+        it("is expired", () => {
+            var app = new MembershipApplication({validUntil: Date.parse("01/01/2010")});
+            assert(app.expired());
+        });
+
+        it('email is 4 characters or less', function () {
+            var app = new MembershipApplication({ email: "dd" });
+            assert(!app.emailIsValid());
+        });
+        it('email does not contain an @', function () {
+            var app = new MembershipApplication({ email: "thingthingthing:thing.com" });
+            assert(!app.emailIsValid());
+        });
+        it('email is omitted', function () {
+            var app = new MembershipApplication();
+            assert(!app.emailIsValid());
+        });
+        it('height is less than 60 inches', function () {
+            var app = new MembershipApplication({ height: 10 });
+            assert(!app.heightIsValid());
+        });
+        it('height is more than 75 inches', function () {
+            var app = new MembershipApplication({ height: 80 });
+            assert(!app.heightIsValid());
+        });
+        it('height is omitted', function () {
+            var app = new MembershipApplication();
+            assert(!app.heightIsValid());
+        });
+        it('age is more than 100', function () {
+            var app = new MembershipApplication({ age: 101 });
+            assert(!app.ageIsValid());
+        });
+        it('age less than 15', function () {
+            var app = new MembershipApplication({ age: 14 });
+            assert(!app.ageIsValid());
+        });
+        it('age is omitted', function () {
+            var app = new MembershipApplication();
+            assert(!app.ageIsValid());
+        });
+        it('weight less than 100', function () {
+            var app = new MembershipApplication({ weight: 99 });
+            assert(!app.weightIsValid());
+        });
+        it('weight less more than 300', function () {
+            var app = new MembershipApplication({ weight: 301 });
+            assert(!app.weightIsValid());
+        });
+        it('weight is omitted', function () {
+            var app = new MembershipApplication({ weight: 301 });
+            assert(!app.weightIsValid());
+        });
+        it('first is omitted', function () {
+            var app = new MembershipApplication();
+            assert(!app.nameIsValid());
+        });
+        it('last is omitted', function () {
+            var app = new MembershipApplication();
+            assert(!app.nameIsValid());
+        });
+
     });
 });
