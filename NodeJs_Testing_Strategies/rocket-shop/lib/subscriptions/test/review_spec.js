619f8f695d837143429766bc8bb28539ebbe7908
jackhwl <jackhwl@hotmail.com>
2019-02-10 17:28:36 -0500

    SinonJs Spy

--- NodeJs_Testing_Strategies/rocket-shop/lib/subscriptions/test/review_spec.js
+++ NodeJs_Testing_Strategies/rocket-shop/lib/subscriptions/test/review_spec.js
@@ -1,20 +1,29 @@
 var assert = require("assert");
 var ReviewProcess = require("../processes/review");
 var MembershipApplication = require("../models/membership_application");
+var sinon = require("sinon");
 
 describe("The Review Process", () => {
     describe("Receiving a valid application", () => {
         var decision;
+        var validApp = new MembershipApplication({
+            first: "Test",
+            last: "User",
+            email: "test@test.com",
+            age: 30,
+            height: 66,
+            weight: 180
+        });
+        var review = new ReviewProcess();
+        var validationSpy = sinon.spy();
+        var missionSpy = sinon.spy();
+        var roleAvailableSpy = sinon.spy();
+        var roleCompatibleSpy = sinon.spy();
         before((done)=>{
-            validApp = new MembershipApplication({
-                first: "Test",
-                last: "User",
-                email: "test@test.com",
-                age: 30,
-                height: 66,
-                weight: 180
-            });
-            var review = new ReviewProcess();
+            review.on("validated", validationSpy);
+            review.on("mission-selected", missionSpy);
+            review.on("role-available", roleAvailableSpy);
+            review.on("role-compatible", roleCompatibleSpy);
             review.processApplication(validApp, (err, result) => {
                 decision = result;
                 done();
@@ -23,6 +32,18 @@ describe("The Review Process", () => {
 
         it('returns success', () => {
             assert(decision.success, decision.message);
+        });
+        it('ensures the application is valid', () => {
+            assert(validationSpy.called);
+        });
+        it('selects a mission', () => {
+            assert(missionSpy.called);
+        });
+        it('ensures a role exists', () => {
+            assert(roleAvailableSpy.called);
+        });
+        it('ensures role compatibility', () => {
+            assert(roleCompatibleSpy.called);
         });
     });
 });
